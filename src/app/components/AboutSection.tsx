21229be067b25a330c3e05a3755827a410a2f947
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-07-17 18:00:30 +0530

    feat: About Me Section

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -1,15 +1,41 @@
 import React from "react";
-import { AboutMeCard } from "@/components/aboutMeSection";
+import {
+  AboutMeCard,
+  AndroidDevCard, SkillsCard,
+  WebDevCard
+} from "@/components/aboutMeSection";
+import AnimatedCard from "@/components/AnimatedCard";
 
 interface AboutSectionProps {
-
   className?: string;
 }
 
 const AboutSection = ({ className = "" }: AboutSectionProps) => {
   return (
     <div className="flex flex-col gap-4 p-4">
-      <AboutMeCard/>
+      <AboutMeCard />
+
+      <div className="flex flex-col gap-4 md:flex-row overflow-x-hidden">
+        <AnimatedCard
+          className="z-10 basis-1/2"
+          initial={{ x: 350 }}
+          whileInView={{ x: 0 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+        >
+          <AndroidDevCard className="h-full" />
+        </AnimatedCard>
+
+        <AnimatedCard
+          className="z-20 basis-1/2"
+          initial={{ x: -350 }}
+          whileInView={{ x: 0 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+        >
+          <WebDevCard className="h-full" />
+        </AnimatedCard>
+      </div>
+
+      <SkillsCard/>
     </div>
   );
 };
