ae4e2c9bec3ca0f5becc9207cc002658d5cecbc9
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-07-22 20:27:04 +0530

    feat: Connect Section

---
+++ src/components/socialSection/index.ts
@@ -0,0 +1,6 @@
+import SocialConnectCard from "@/components/socialSection/SocialConnectCard";
+import DSACard from "@/components/socialSection/DSACard";
+import BlogsCard from "@/components/socialSection/BlogsCard";
+import ContactForm from "@/components/socialSection/contactForm/ContactForm";
+
+export { SocialConnectCard, DSACard, BlogsCard, ContactForm }
