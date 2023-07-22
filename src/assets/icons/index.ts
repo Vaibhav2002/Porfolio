de2d3fbdd154f4441324b09a0d9c57fa76005e61
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-07-22 20:26:51 +0530

    add: Icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,11 @@
+import CodeChef from "@/assets/icons/Codechef";
+import LeetCode from "@/assets/icons/LeetCode";
+import GeeksForGeeks from "@/assets/icons/GeeksForGeeks";
+
+class Icons {
+  static readonly CodeChef = CodeChef
+  static readonly LeeetCode = LeetCode
+  static readonly GeeksForGeeks = GeeksForGeeks
+}
+
+export default Icons
