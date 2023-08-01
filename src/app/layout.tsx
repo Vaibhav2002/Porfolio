9ecff4c7e9abf516f644e70d4a8d331ef03a2308
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-08-01 16:27:01 +0530

    add: Twitter large summary card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const dmSans = DM_Sans({ weight: ["400", "500", "700"], subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Vaibhav Jaiswal",
   description: "I am Vaibhav Jaiswal, a Computer Science Engineering student, currently working as a Software Engineer Intern at Pratilipi.",
+  twitter: {
+    card: "summary_large_image",
+  }
 };
 
 export default function RootLayout({
