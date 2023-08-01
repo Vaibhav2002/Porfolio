aeb7800b2693efefb293fb817eb8aade691260fb
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-08-01 15:23:49 +0530

    refac: Projects Card responsive

--- src/components/projectsAndAwardsSection/ProjectsCard.tsx
+++ src/components/projectsAndAwardsSection/ProjectsCard.tsx
@@ -27,8 +27,8 @@ const ProjectsGrid = () => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8">
-      {projects.map(project => (
-        <ProjectItem project={project} key={project.url}/>
+      {projects.map((project, index) => (
+        <ProjectItem project={project} key={project.url} className={index>5 ? "hidden lg:flex" : ''}/>
       ))}
     </div>
   )
